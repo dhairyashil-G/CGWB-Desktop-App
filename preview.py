9cdc5bdad603e3294ddf3882b27c297011e50912
Dhairyashil <dhairyasheel.ghatage@gmail.com>
2023-09-22 17:31:01 +0530

    preview table adjusted

--- preview.py
+++ preview.py
@@ -1,6 +1,7 @@
 from PyQt5 import uic,QtWebEngineWidgets
 from multiPageHandler import PageWindow
 from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtWidgets import QTableWidgetItem
 import sqlite3
 import pandas as pd
 import plotly.graph_objs as go
@@ -15,6 +16,11 @@ class PreviewPage(PageWindow, QObject):
         well_id_global=None
         self.plot_button.clicked.connect(self.show_plot)
         # self.show_plot()
+
+        self.pumping_table.setColumnWidth(0, 300)  # Set minimum width for column 0 to 100 pixels
+        self.pumping_table.setColumnWidth(1, 300)
+        self.recovery_table.setColumnWidth(0, 300) 
+        self.recovery_table.setColumnWidth(1, 300)  
 
     def show_plot(self):
         conn = sqlite3.connect('database.db')
@@ -42,10 +48,21 @@ class PreviewPage(PageWindow, QObject):
         t_when_pumping_stopped = well_object.get('TimeWhenPumpingStopped', 0)
         # t_when_pumping_stopped = 240
         csv_file_url = well_object.get('CsvFilePath', '')
+        print(csv_file_url)
         df = pd.read_csv(csv_file_url)
         df_pumping_test = df[df['Time'] <= t_when_pumping_stopped]
         df_recovery_test = df[df['Time'] > t_when_pumping_stopped]
-
+        
+        for i, row in enumerate(df_pumping_test.itertuples(index=False)):
+            for j, item in enumerate(row):
+                item_data= QTableWidgetItem(str(item))
+                self.pumping_table.setItem(i, j,item_data)
+        
+        for i, row in enumerate(df_recovery_test.itertuples(index=False)):
+            for j, item in enumerate(row):
+                item_data= QTableWidgetItem(str(item))
+                self.recovery_table.setItem(i, j,item_data)
+        
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=df_pumping_test['Time'], y=df_pumping_test['Drawdown'],
                                  mode='lines+markers',
