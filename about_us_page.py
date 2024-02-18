815e523e0316cb87830712df99754887d7592a80
Sahil Gidwani <sahilgdwn@gmail.com>
2024-02-18 12:52:03 +0530

    refactor and format about us

--- about_us_page.py
+++ about_us_page.py
@@ -3,17 +3,30 @@ from PyQt5 import QtCore
 from PyQt5 import uic
 from multiPageHandler import PageWindow
 
+
 class AboutUsPage(PageWindow):
     def __init__(self):
-        super(AboutUsPage,self).__init__()
-        uic.loadUi('about_us.ui',self)
+        super(AboutUsPage, self).__init__()
+        self.setup_ui()
         self.setWindowTitle('AquaProbe')
-        self.statusbar.showMessage("Version 1.0.0")
-        copyright_label = QLabel("Copyright © 2024 AquaProbe. All rights reserved.")
-        copyright_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
-        self.statusbar.showMessage("Version 1.0.0")
+        self.set_statusbar_message("Version 1.0.0")
+        self.add_copyright_label()
+        self.setup_connections()
+
+    def setup_ui(self):
+        uic.loadUi('about_us.ui', self)
+
+    def set_statusbar_message(self, message):
+        self.statusbar.showMessage(message)
+
+    def add_copyright_label(self):
+        copyright_label = QLabel(
+            "Copyright © 2024 AquaProbe. All rights reserved.")
+        copyright_label.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
         self.statusbar.addPermanentWidget(copyright_label)
 
+    def setup_connections(self):
         self.back_button.clicked.connect(self.goback)
         self.menuWellTable.aboutToShow.connect(self.goto_welltable)
         self.menuHome.aboutToShow.connect(self.goto_home)
