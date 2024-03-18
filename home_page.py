8d3fb7d38f1a32186bf7a5b3dfee215884e9e08d
Dhairyashil Anil Ghatage <dhairyasheel.ghatage@gmail.com>
2024-03-18 14:04:20 +0530

    Updates on home page

--- home_page.py
+++ home_page.py
@@ -1,7 +1,7 @@
 from PyQt5 import uic, QtCore
 from multiPageHandler import PageWindow
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLabel, QMessageBox
 
 
 class HomePage(PageWindow):
@@ -23,6 +23,7 @@ class HomePage(PageWindow):
         self.createwell_button.clicked.connect(self.goto_createwell)
         self.welltable_button.clicked.connect(self.goto_welltable)
         self.menuWellTable.aboutToShow.connect(self.goto_welltable)
+        self.review_terms_button.clicked.connect(self.show_terms_of_use)
         self.menuHome.aboutToShow.connect(self.goto_home)
         self.menuAbout.aboutToShow.connect(self.goto_aboutus)
         self.menuHelp.aboutToShow.connect(self.goto_help)
@@ -30,6 +31,7 @@ class HomePage(PageWindow):
 
     def setup_carousel(self):
         self.images = [
+            QPixmap("docs/flowchart.svg"),
             QPixmap("Home_image1.gif"),
             QPixmap("carousel_images/C0.jpg"),
             QPixmap("carousel_images/C1.jpg"),
@@ -69,3 +71,29 @@ class HomePage(PageWindow):
         if self.currentIndex >= len(self.images):
             self.currentIndex = 0
         self.carousel_images_label.setPixmap(self.images[self.currentIndex])
+    
+    def show_terms_of_use(self):
+        terms_text = """
+        <html><head/><body><p>
+        <b>Terms of Use</b><br><br>
+        Please read the terms carefully before using the AquaProbe application. By downloading, installing, or using the Software, you agree to be bound by these terms. If you do not agree to these terms, do not use the Software.<br><br>
+        
+        <b>License:</b> The Software is provided as a freeware under a non-exclusive, non-transferable license to use it solely for personal or internal use purposes. You may not sublicense, sell, or distribute the software without prior written consent.<br><br>
+        
+        <b>Restrictions:</b> You may not modify, adapt, translate, reverse engineer, decompile, disassemble, or create derivative works based on the software.<br><br>
+        
+        <b>Ownership:</b> All rights, title, and interest in and to the Software, including any updates, enhancements, or modifications, remain with team members (SIH2022-BV798) and CGWB, Department of Water Resources, River Development and Ganga Rejuvenation, Ministry of Jal Shakti.<br><br>
+        
+        <b>Changes to Terms:</b> AquaProbe (SIH2022-BV798) reserves the right to change these terms at any time without notice. Your continued use of the Software after any such changes constitutes your acceptance of the new terms.<br><br>
+        
+        <b>Citation:</b> The use of the software may be cited as follows: Surname, Initial. (Year). AquaProbe - a desktop application for analysis of pumping test data developed under SIH 2022, Central Ground Water Board (CGWB) Ministry of Jal Shakti, Govt of India.<br><br>
+        
+        By using the Software, you acknowledge that you have read and understood these terms and agree to be bound by them. If you have any questions about these terms, please contact us at <a href="mailto:aquaprobe.help@gmail.com">aquaprobe.help@gmail.com</a>.
+        </p></body></html>
+        """
+        msgBox = QMessageBox()
+        msgBox.setTextFormat(1)  # Rich text format
+        msgBox.setText(terms_text)
+        msgBox.setWindowTitle("Terms of Use")
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.exec()
